abcaeb9a26fceda0c85386af4fc4f97e58e74a27
Justyna Małuszyńska <64073445+justyna-maluszynska@users.noreply.github.com>
2022-10-13 19:07:04 +0200

    Get rides with parameters

--- users/models.py
+++ users/models.py
@@ -6,3 +6,4 @@ class User(models.Model):
     first_name = models.CharField(max_length=100, null=False)
     last_name = models.CharField(max_length=100, null=False)
     avg_rate = models.DecimalField(max_digits=3, decimal_places=2)
+    private = models.BooleanField(null=False, default=True)
