1e58e460e87b9fd2e6f64baffcdf3712196e7659
Justyna Małuszyńska <64073445+justyna-maluszynska@users.noreply.github.com>
2022-10-28 10:03:04 +0200

    Add .env.example

--- rides/utils/validate_token.py
+++ rides/utils/validate_token.py
@@ -1,3 +1,4 @@
+import os
 from functools import wraps
 
 import jwt
@@ -14,7 +15,7 @@ def validate_token(func):
             return JsonResponse(data='Invalid token provided', status=status.HTTP_401_UNAUTHORIZED, safe=False)
 
         try:
-            public_key = """-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApxHd1H4JBJx0rbKg9D8+qzHyjhoDK+h4Ht+FIQsaLKEMDqs+Q8izMRrlcpCqXvoNylZd+N0sJHepwR1sxlXsELGZlJr1MZLXBe5YMNVXXbOJzVJQZcEgaDOgL+mmPRNb9OqJG4TecmGi+sdw6o5OHI14X2hsOpLJ1hlyetZOX6cx23/pxAfpgTRfrdPWZGDOlrWTktPr5Dx3+lWXYmwXBdaqM284Lw2VIoGKQvuADshlWtYks+m2L8VrDOYpehFhQ9vbpyfg0+zVdOM+VtmWPAqLFDRBXBfVmmqDkIOZs53STIhlrywvhEuKlWLvhToGWsKimoomYSobUJj40NlALQIDAQAB\n-----END PUBLIC KEY-----"""
+            public_key = f"""-----BEGIN PUBLIC KEY-----\n{os.environ.get("TOKEN_KEY")}\n-----END PUBLIC KEY-----"""
             decoded_token = jwt.decode(token, public_key, algorithms=["RS256"], audience="account")
         except jwt.exceptions.DecodeError:
             return JsonResponse(data='Token decoding failure', status=status.HTTP_401_UNAUTHORIZED, safe=False)
