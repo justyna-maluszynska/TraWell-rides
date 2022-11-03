c8c4e93b4eb03448ab2cd2dd1dc9f26ba8307190
Justyna Małuszyńska <64073445+justyna-maluszynska@users.noreply.github.com>
2022-11-03 23:06:15 +0100

    First tests

--- rides/models.py
+++ rides/models.py
@@ -94,10 +94,10 @@ class Ride(models.Model):
     def save(self, *args, **kwargs):
         if not self.ride_id:
             super(Ride, self).save(*args, **kwargs)
-        if not self.available_seats:
-            self.available_seats = self.get_available_seats
+        # if not self.available_seats:
+        #     self.available_seats = self.get_available_seats
         self.available_seats = self.get_available_seats
-        super(Ride, self).save(*args, **kwargs)
+        super(Ride, self).save()
 
 
 class Participation(models.Model):
@@ -114,12 +114,12 @@ class Participation(models.Model):
 
     def delete(self, using=None, keep_parents=False):
         super(Participation, self).delete(using, keep_parents)
-        self.ride.available_seats = self.ride.get_available_seats
+        # self.ride.available_seats = self.ride.get_available_seats
         self.ride.save()
 
     def save(self, *args, **kwargs):
         super(Participation, self).save(*args, **kwargs)
-        self.ride.available_seats = self.ride.get_available_seats
+        # self.ride.available_seats = self.ride.get_available_seats
         self.ride.save()
 
 
--- rides/tests.py
+++ rides/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rides.factories import RideFactory, ParticipationFactory
+from rides.models import Ride, Participation
+
+
+class RideModelTests(TestCase):
+    def test_ride_default_available_seats(self):
+        ride_factory = RideFactory.create(seats=5)
+        ride = Ride.objects.get(ride_id=ride_factory.ride_id)
+
+        self.assertEqual(ride.available_seats, 5)
+
+    def test_updates_available_seats(self):
+        ride_factory = RideFactory.create(seats=5)
+        participation1 = ParticipationFactory.create(ride=ride_factory, decision=Participation.Decision.ACCEPTED,
+                                                     reserved_seats=2)
+        ride = Ride.objects.get(ride_id=ride_factory.ride_id)
+
+        self.assertEqual(ride.available_seats, 3)
+
+        participation2 = ParticipationFactory.create(ride=ride_factory, decision=Participation.Decision.PENDING,
+                                                     reserved_seats=1)
+        ride.refresh_from_db()
+        self.assertEqual(ride.available_seats, 2)
+
+        participation1.decision = Participation.Decision.CANCELLED
+        participation1.save()
+        ride.refresh_from_db()
+        self.assertEqual(ride.available_seats, 4)
+
+        participation2.decision = Participation.Decision.CANCELLED
+        participation2.save()
+        ride.refresh_from_db()
+        self.assertEqual(ride.available_seats, 5)
