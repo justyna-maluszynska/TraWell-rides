6079c4f3211aacf786e36e64053e7f40a819bd2d
LeviSforza <3monika100@gmail.com>
2022-11-25 21:02:56 +0100

    Update create_manual_data.py

--- scripts/create_manual_data.py
+++ scripts/create_manual_data.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 
 import scripts.create_cities
 from cities.models import City
@@ -11,47 +11,77 @@ from users.models import User
 from vehicles.models import Vehicle
 
 rides = [
-    {'city_from': 1,    # id of city from db
-     'city_to': 2,  # id of city from db
+    {'city_from': 5,  # id of city from db
+     'city_to': 1,  # id of city from db
      'area_from': 'fromfromfrom',
      'area_to': 'totototo',
-     'start_date': datetime(2022, 12, 20, 9),
-     'duration': datetime.timedelta(seconds = 3600),
+     'start_date': datetime.datetime(2022, 12, 20, 20),
+     'duration': datetime.timedelta(seconds=3600),
      'price': 20.50,
      'seats': 3,
      'recurrent': False,
      'automatic_confirm': False,
      'description': 'lalala',
-     'driver_email': 'anna.nowak@wp.pl',
+     'driver_email': 'anna@sowa.com',
      'is_cancelled': False,
      'recurrent_ride': None}
 ]
 
-
 participations = [
     {'ride_id': 1,
-     'user_email': 'newmail@gmail.com',
+     'user_email': 'jan@kot.com',
      'decision': 'accepted',
      'reserved_seats': 2}
 ]
 
-
 recurrent_rides = [
-    {'city_from': 1,    # id of city from db
-     'city_to': 2,  # id of city from db
-     'area_from': 'fromfromfrom',
-     'area_to': 'totototo',
-     'start_date': datetime(2022, 12, 1, 9),
-     'end_date': datetime(2023, 1, 30, 9),
-     'frequency_type': 'weekly',
+    {'city_from': 5,  # id of city from db
+     'city_to': 1,  # id of city from db
+     'area_from': 'park',
+     'area_to': 'bar',
+     'start_date': datetime.datetime(2022, 11, 1, 18),
+     'end_date': datetime.datetime(2023, 1, 10, 19),
+     'frequency_type': 'daily',
      'frequence': 1,
-     'occurences': None,
-     'duration': datetime.timedelta(seconds=3600),
-     'price': 20.50,
-     'seats': 3,
+     'occurrences': None,
+     'duration': datetime.timedelta(seconds=2100),
+     'price': 26.99,
+     'seats': 6,
      'automatic_confirm': False,
-     'description': 'lalala',
-     'driver_email': 'anna.nowak@wp.pl',
+     'description': 'Best ride ever',
+     'driver_email': 'anna@sowa.com',
+     'is_cancelled': False},
+    {'city_from': 5,  # id of city from db
+     'city_to': 1,  # id of city from db
+     'area_from': 'ul. Warszawska',
+     'area_to': 'dom kultury',
+     'start_date': datetime.datetime(2022, 11, 26, 16),
+     'end_date': datetime.datetime(2023, 2, 1, 20),
+     'frequency_type': 'daily',
+     'frequence': 1,
+     'occurrences': None,
+     'duration': datetime.timedelta(seconds=5000),
+     'price': 5.99,
+     'seats': 5,
+     'automatic_confirm': True,
+     'description': 'Super rides for everyone',
+     'driver_email': 'jan@kot.com',
+     'is_cancelled': False},
+    {'city_from': 2,  # id of city from db
+     'city_to': 3,  # id of city from db
+     'area_from': 'ul. Mazowiecka 55a',
+     'area_to': 'przystanek na Wesołej',
+     'start_date': datetime.datetime(2022, 11, 1, 14),
+     'end_date': datetime.datetime(2023, 1, 24, 14),
+     'frequency_type': 'daily',
+     'frequence': 1,
+     'occurrences': None,
+     'duration': datetime.timedelta(seconds=800),
+     'price': 67.32,
+     'seats': 5,
+     'automatic_confirm': False,
+     'description': '<3',
+     'driver_email': 'anna@sowa.com',
      'is_cancelled': False}
 ]
 
@@ -60,7 +90,7 @@ def create_manual_rides(rides):
     for ride in rides:
         city_from = City.objects.get(city_id=ride['city_from'])
         city_to = City.objects.get(city_id=ride['city_to'])
-        driver = User.objects.get(email=ride['email'])
+        driver = User.objects.get(email=ride['driver_email'])
         vehicles = Vehicle.objects.filter(user=driver)
         if len(vehicles) > 0:
             vehicle = vehicles[0]
@@ -83,7 +113,7 @@ def create_manual_rides(rides):
                                        recurrent_ride=ride['recurrent_ride'])
         new_ride.save()
 
-        serializer = RideSerializer(ride)
+        serializer = RideSerializer(new_ride)
         tasks.publish_message(serializer.data, 'rides.create', queue_notify, 'notify')
         tasks.publish_message(serializer.data, 'rides.create', queue_reviews, 'review')
 
@@ -105,7 +135,7 @@ def create_manual_recurrent_rides(recurrent_rides):
     for ride in recurrent_rides:
         city_from = City.objects.get(city_id=ride['city_from'])
         city_to = City.objects.get(city_id=ride['city_to'])
-        driver = User.objects.get(email=ride['email'])
+        driver = User.objects.get(email=ride['driver_email'])
         vehicles = Vehicle.objects.filter(user=driver)
         if len(vehicles) > 0:
             vehicle = vehicles[0]
@@ -119,7 +149,7 @@ def create_manual_recurrent_rides(recurrent_rides):
                                                 end_date=ride['end_date'],
                                                 frequency_type=ride['frequency_type'],
                                                 frequence=ride['frequence'],
-                                                occurences=ride['occurences'],
+                                                occurrences=ride['occurrences'],
                                                 duration=ride['duration'],
                                                 price=ride['price'],
                                                 seats=ride['seats'],
