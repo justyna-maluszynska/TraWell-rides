f70462f19f0fe50ace5d835ff8eedc09a5ca31df
Justyna Małuszyńska <64073445+justyna-maluszynska@users.noreply.github.com>
2022-12-05 18:01:08 +0100

    celery utils fix

--- utils/celery_utils.py
+++ utils/celery_utils.py
@@ -5,14 +5,24 @@ from vehicles.models import Vehicle
 def create_user(message):
     is_private = True if message['user_type'] == 'private' else False
     message['private'] = is_private
-    user, _ = User.objects.update_or_create(user_id=message['user_id'], defaults=message)
+    user, _ = User.objects.update_or_create(user_id=message['user_id'], defaults={'first_name': message['first_name'],
+                                                                                  'last_name': message["last_name"],
+                                                                                  'email': message["email"],
+                                                                                  'avatar': message['avatar'],
+                                                                                  'private': True if message[
+                                                                                                         'user_type'] == 'private' else False,
+                                                                                  'avg_rate': message['avg_rate']})
     return user
 
 
 def create_vehicle(message):
     user_data = message.pop('user')
     user, _ = create_user(user_data)
-    vehicle, _ = Vehicle.objects.update_or_create(vehicle_id=message['vehicle_id'], user=user, defaults=message)
+    vehicle, _ = Vehicle.objects.update_or_create(vehicle_id=message['vehicle_id'], user=user,
+                                                  defaults={'make': message['make'],
+                                                            'model': message['model'],
+                                                            'color': message['color'],
+                                                            })
     return vehicle
 
 
